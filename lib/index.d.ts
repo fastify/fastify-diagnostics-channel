a1be6d5f3aab001c2f48a186b1f13f852b9be158
RafaelGSS <rafael.nunu@hotmail.com>
2020-12-11 15:51:30 -0300

    feat: add and export types to plugin

---
+++ lib/index.d.ts
@@ -0,0 +1,24 @@
+import { RouteOptions, HTTPMethods } from 'fastify';
+
+export declare interface OnRouteEvent extends RouteOptions { }
+
+export declare interface OnTimeoutEvent {
+  protocol: 'http' | 'https';
+  url: string;
+  method: HTTPMethods | HTTPMethods[];
+}
+
+export declare interface OnErrorEvent {
+  protocol: 'http' | 'https';
+  url: string;
+  method: HTTPMethods | HTTPMethods[];
+  error: Error;
+}
+
+export declare interface OnResponseEvent {
+  protocol: 'http' | 'https';
+  url: string;
+  method: HTTPMethods | HTTPMethods[];
+  statusCode: number;
+  headers: string[];
+}
